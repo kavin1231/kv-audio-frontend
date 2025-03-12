90d56d092a9e366f843be484f4dd5ce927c143a7
kavin1231 <chamathkaveendra@gmail.com>
2025-03-12 16:01:28 +0530

    add tailwind css and add dashboard❇️❇️❇️

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { BsGraphDown } from "react-icons/bs";
 import "./App.css";
 import ProductCard from "./components/productCard";
+import { GoGraph } from "react-icons/go";
+import { CiBookmark } from "react-icons/ci";
+import { CiSpeaker } from "react-icons/ci";
+import { CiUser } from "react-icons/ci";
 
 function App() {
-  return <div className="test testTwo">fdsf</div>;
+  return (
+    <div className="w-full h-screen flex">
+      <div className="w-[400px] h-full bg-green-200">
+          <button className="w-full h-[40px] text-black text-[25px] font-bold flex justify-center items-center">
+            <BsGraphDown/>
+            Dashboard
+          </button>
+          <button className="w-full h-[40px] text-black text-[25px] font-bold flex justify-center items-center">
+          <CiBookmark />
+            Booking
+          </button>
+          <button className="w-full h-[40px] text-black text-[25px] font-bold flex justify-center items-center">
+          <CiSpeaker />
+            Items
+          </button>
+          <button className="w-full h-[40px] text-black text-[25px] font-bold flex justify-center items-center">
+          <CiUser />
+            Users
+          </button>
+
+      </div>
+      <div className="w-full bg-red-900">
+
+
+      </div>
+    </div>
+  );
 }
 
 export default App;
