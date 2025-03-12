e2aba845db596f34cf9c96b28301ebef6b95d9c0
kavin1231 <chamathkaveendra@gmail.com>
2025-03-12 16:10:28 +0530

    create adminpage component

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,12 @@ import { GoGraph } from "react-icons/go";
 import { CiBookmark } from "react-icons/ci";
 import { CiSpeaker } from "react-icons/ci";
 import { CiUser } from "react-icons/ci";
+import AdminPage from "./components/adminPage";
 
 function App() {
   return (
-    <div className="w-full h-screen flex">
-      <div className="w-[400px] h-full bg-green-200">
-          <button className="w-full h-[40px] text-black text-[25px] font-bold flex justify-center items-center">
-            <BsGraphDown/>
-            Dashboard
-          </button>
-          <button className="w-full h-[40px] text-black text-[25px] font-bold flex justify-center items-center">
-          <CiBookmark />
-            Booking
-          </button>
-          <button className="w-full h-[40px] text-black text-[25px] font-bold flex justify-center items-center">
-          <CiSpeaker />
-            Items
-          </button>
-          <button className="w-full h-[40px] text-black text-[25px] font-bold flex justify-center items-center">
-          <CiUser />
-            Users
-          </button>
-
-      </div>
-      <div className="w-full bg-red-900">
-
-
-      </div>
+    <div>
+      <AdminPage />
     </div>
   );
 }
